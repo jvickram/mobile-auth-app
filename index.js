974d1a675876070625684dc23d59500c574cc966
Bickey Jaisawal <jvickram2@gmail.com>
2020-06-07 14:16:30 +0530

    Added common component and created a app.js file to make use of it

--- index.js
+++ index.js
@@ -2,14 +2,9 @@
  * @format
  */
 
-import React from 'react'
-import {AppRegistry, Text} from 'react-native';
+import {AppRegistry} from 'react-native';
 
-// import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
-
-const App = () => (
-    <Text>Mobile Authentication App</Text>
-)
 
 AppRegistry.registerComponent(appName, () => App);
